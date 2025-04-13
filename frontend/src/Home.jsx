5e71dfe6db42de0293944d7b27b01d85b64819b0
Slava <splakhin@yahoo.com>
2025-04-13 17:05:59 -0500

    added favicon

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -17,8 +17,8 @@ export default function Home() {
 
   const handleHomeClick = () => navigate('/');
   const handleContactClick = () => navigate('/contact');
-  const handleAboutUsClick = () => navigate('/about_us');
-  const handleSettingsClick = () => { sessionStorage.clear(); navigate('/user'); }
+  const handleJenkinsClick = () => navigate('/about_us');
+  const handleDbClick = () => { sessionStorage.clear(); navigate('/user'); }
 
   return (
     <div className="home-container">
@@ -29,9 +29,9 @@ export default function Home() {
           {/*<a href="https://www.freepik.com/icon/house-chimney_9243286">Icon by UIcons</a>*/}
           <img src="/img/contactUs_16769119.png" alt="Contact" className={`bar-icon w-8 h-8 cursor-pointer ${currentLoc === "/contact" ? "dim-icon" : ""}`} onClick={handleContactClick} />
           {/*<a href="https://www.freepik.com/icon/clip-mail_16769119#fromView=search&page=3&position=31&uuid=43f68ff2-180d-4ce0-9887-8f952acbaf88">Icon by UIcons</a> */}
-          <img src="/img/aboutUs_5529124.png" alt="CI" className={`bar-icon w-8 h-8 cursor-pointer ${currentLoc === "/about_us" ? "dim-icon" : ""}`} onClick={handleAboutUsClick} />
+          <img src="/img/jenkins_5529124.png" alt="CI" className={`bar-icon w-8 h-8 cursor-pointer ${currentLoc === "/about_us" ? "dim-icon" : ""}`} onClick={handleJenkinsClick} />
           {/*<a href="https://www.freepik.com/icon/users-alt_5529124#fromView=search&page=5&position=51&uuid=9e36625c-161e-4109-b420-ccbe393fc2ea">Icon by UIcons</a>*/}
-          <img src="/img/settings_16311406.png" alt="DB" className={`bar-icon w-8 h-8 cursor-pointer ${currentLoc === "/user" ? "dim-icon" : ""}`} onClick={handleSettingsClick} />
+          <img src="/img/settings_16311406.png" alt="DB" className={`bar-icon w-8 h-8 cursor-pointer ${currentLoc === "/user" ? "dim-icon" : ""}`} onClick={handleDbClick} />
           {/* <a href="https://www.freepik.com/icon/user-skill-gear_16311406#fromView=search&page=5&position=3&uuid=9e36625c-161e-4109-b420-ccbe393fc2ea">Icon by UIcons</a> */}
         </div>
       </header>
@@ -80,8 +80,8 @@ export default function Home() {
 //
 //   const handleHomeClick = () => {navigate('/'); }
 //   const handleContactClick = () => navigate('/contact');
-//   const handleAboutUsClick = () => navigate('/about_us');
-//   const handleSettingsClick = () => {sessionStorage.clear(); navigate('/user'); }
+//   const handleJenkinsClick = () => navigate('/about_us');
+//   const handleDbClick = () => {sessionStorage.clear(); navigate('/user'); }
 //
 //   return (
 //     <div className={`home-container ${isHome ? "dim-background" : ""}`} >
@@ -90,8 +90,8 @@ export default function Home() {
 //         <div className="flex items-center space-x-4">
 //           <img src="/img/home-b.png"   alt="Home"     className={`bar-icon w-8 h-8 cursor-pointe; ${isHome ? "" : "dim-icon"}`} onClick={handleHomeClick}  />
 //           <img src="/img/contact.png"  alt="Contact"  className={`bar-icon w-8 h-8 cursor-pointe; ${isHome ? "dim-icon" : ""}`} onClick={handleContactClick} />
-//           <img src="/img/aboutUs.png"  alt="About Us" className={`bar-icon w-8 h-8 cursor-pointe; ${isHome ? "dim-icon" : ""}`} onClick={handleAboutUsClick} />
-//           <img src="/img/settings.png" alt="Settings" className={`bar-icon w-8 h-8 cursor-pointe; ${isHome ? "dim-icon" : ""}`} onClick={handleSettingsClick} />
+//           <img src="/img/aboutUs.png"  alt="About Us" className={`bar-icon w-8 h-8 cursor-pointe; ${isHome ? "dim-icon" : ""}`} onClick={handleJenkinsClick} />
+//           <img src="/img/settings.png" alt="Settings" className={`bar-icon w-8 h-8 cursor-pointe; ${isHome ? "dim-icon" : ""}`} onClick={handleDbClick} />
 //         </div>
 //       </div>
 //
