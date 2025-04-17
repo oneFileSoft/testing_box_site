332d29feaf00eab29db2f82a3e8a5393333af1a8
Slava <splakhin@yahoo.com>
2025-04-17 14:56:17 -0500

    new chages

--- server.js
+++ server.js
@@ -86,8 +86,11 @@ app.post('/send-email', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+//app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+//});
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 // Start the server
