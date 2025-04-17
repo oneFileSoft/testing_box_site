2c957e31c6e4d31875bc96a1b2dfd2e05038e4f8
Slava <splakhin@yahoo.com>
2025-04-17 09:14:31 -0500

    new chages

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const corsOptions = {
     allowedHeaders: ['Content-Type'],
 };
 app.use(cors(corsOptions));
-app.use(express.json());
-//app.use(bodyParser.json({ limit: '5mb' }));
-
+//app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 const loginRoute = require('./routes/login');
 app.use('/', loginRoute);
