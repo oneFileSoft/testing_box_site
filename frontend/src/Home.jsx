560078beb6134317c894dc5cdc5a657fb0dcfd58
Slava <splakhin@yahoo.com>
2025-04-19 18:16:36 -0500

    new chages

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -41,7 +41,7 @@ export default function Home() {
 
       {/* Body height: `${isRegr? '70%' : '30%'}` */}
       <main className={`main-content ${isHome ? "dim-background" : ""}`}   style={{ height: '97%' }}>
-        <div className="content-container" style={{ display: 'flex', flexDirection: 'column', width: '100%', height: `${isRegr? '100%' : '100%'}` }}>
+        <div className="content-container" style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%' }}>
           <div style={{ width: '90%', height: '100%', alignItems: 'center', justifyContent: 'center' }} class="min-h-0">
             <Outlet />
           </div>
