148e37a47295e3765a94c73417cebc453c51bcd4
Slava <splakhin@yahoo.com>
2025-04-26 10:18:01 -0500

    new chages

--- frontend/src/RegrReport.jsx
+++ frontend/src/RegrReport.jsx
@@ -142,7 +142,7 @@ export default function RegrReport() {
     style={{
       display: 'flex',
       flexDirection: 'column',
-      height: '90vh',
+      height: '85vh',
       border: '1px solid #ccc',
       background: '#fff',
       padding: 0,
