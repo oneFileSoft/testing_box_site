4f005bde9fc2e4893e079e1834c71fccc6f45df0
Slava <splakhin@yahoo.com>
2025-05-06 07:45:13 -0500

    new chages

--- frontend/src/AboutUs.jsx
+++ frontend/src/AboutUs.jsx
@@ -11,7 +11,33 @@ const steps = [
 ];
 
 const details = [
-  "vvvvvvvvvvvvPulls both the Web and Test repositories onto the Jenkins agent host.",
+  "Jenkins monitors the release branch of the Web repository. Upon detecting a push event, it performs the following",
+  "Pulls both the Web and Test repositories onto the Jenkins agent host:\n"+
+  "1. Generate PAT from GitHub\n(Profile -> Settings-> left.sidebar.Developer Settings->PersonalAccessToken ->\n"+
+  "   select Fine-grained tokens -> Generate):\n"+,
+  "   Select the scopes (permissions) you want to grant the token. For example, for accessing repositories, you\n"+
+  "   would select Repo.\n"+
+  "   In the Token setting, define repository accessibility, token-expiration time and some permissions\n"+
+  "   *** Click Generate token and copy it, cause it's going to be shown only 1 time !!! ***\n"+
+  "2. Later, in Jenkins (Manage Jenkins > Manage Credentials) we'll need to grant the minimum required\n"+
+  "   permissions for Jenkins to interact with the Repository Permissions. Below are the permissions Jenkins will\n"+
+  "   require:\n"+
+  `Permission	Why is it needed?	Level
+        Metadata (Mandatory)	Allows Jenkins to list repositories and access basic info.	(Pre-selected, cannot be changed)
+        Contents	Allows Jenkins to clone and fetch the repository code.	Read-only
+        Actions	If using GitHub Actions for CI/CD, Jenkins might need this to trigger workflows.	Read-only or None
+        Commit statuses	Allows Jenkins to update commit status (e.g., mark builds as passed/failed).	Read & Write
+        Pull requests	If Jenkins is verifying PRs, this allows it to post build results.	Read-only or Read & Write
+        Deployments (optional)	If Jenkins is deploying your app (e.g., to a server), this is needed.	Read & Write (Only if deploying)
+        Secrets (optional)	Needed if your build uses GitHub Actions secrets.	None unless needed
+
+        Account Permissions
+        Most Account Permissions are NOT needed for Jenkins. However, if your pipeline interacts with GitHub users, consider these:
+        Permission	Why is it needed?	Level
+        Email addresses	If Jenkins needs user email info for notifications.	None
+        Followers	Not needed.	None
+        Private repository invitations	Not needed unless working with private repos.	None
+`,
   "sssssssssssLaunches the Web application locally on the Jenkins environment.",
   "eeeeeeeeeeeeeeeeExecutes regression tests from the Test repository against the deployed Web application.",
   "gggggggggggggggSends the regression test report back to the developer.",
